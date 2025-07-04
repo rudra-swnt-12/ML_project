f2b4fa251ce097f3114543b83f27a30eb158dc1d
rudra-swnt-12 <rudra.vksawnt@gmail.com>
2025-07-04 11:10:12 +0530

    Add model training

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -1,12 +1,17 @@
 import os
 import sys
-from types import TracebackType
-from src.logger import logging
 from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -48,6 +53,11 @@ class DataIngestion:
             raise CustomException(e, sys) from e #type: ignore[no-untyped-call, no-redef]
 
 if __name__ == '__main__':
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
-    print("Data ingestion completed successfully.")
+    obj=DataIngestion()
+    train_data, test_data, raw_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
